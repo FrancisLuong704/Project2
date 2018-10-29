67f3b1c45ebf963f4b48f77ae6addf9245a8a979
YoungFrank <fluong95@gmail.com>
2018-10-29 11:09:54 -0400

    seeds.sql

--- models/seeds.sql
+++ models/seeds.sql
@@ -19,3 +19,53 @@ INSERT INTO transactions (type,category,amount,date)
 values ("deposit","investment",200,"2018-07-01");
 INSERT INTO transactions (type,category,amount,date) 
 values ("deposit","investment",200,"2018-09-01");
+
+INSERT INTO transactions (type,category,amount,date) 
+values ("expense","grocery",50,"2018-04-01");
+INSERT INTO transactions (type,category,amount,date) 
+values ("expense","grocery",50,"2018-05-01");
+INSERT INTO transactions (type,category,amount,date) 
+values ("expense","grocery",50,"2018-06-01");
+INSERT INTO transactions (type,category,amount,date) 
+values ("expense","grocery",50,"2018-07-01");
+INSERT INTO transactions (type,category,amount,date) 
+values ("expense","grocery",50,"2018-08-01");
+INSERT INTO transactions (type,category,amount,date) 
+values ("expense","grocery",50,"2018-09-01");
+INSERT INTO transactions (type,category,amount,date) 
+values ("expense","grocery",50,"2018-10-01");
+
+INSERT INTO transactions (type,category,amount,date) 
+values ("expense","personal care",50,"2018-04-30");
+INSERT INTO transactions (type,category,amount,date) 
+values ("expense","personal care",50,"2018-06-30");
+INSERT INTO transactions (type,category,amount,date) 
+values ("expense","personal care",50,"2018-08-30");
+INSERT INTO transactions (type,category,amount,date) 
+values ("expense","personal care",50,"2018-10-30");
+
+INSERT INTO transactions (type,category,amount,date) 
+values ("expense","saving",50,"2018-07-15");
+INSERT INTO transactions (type,category,amount,date) 
+values ("expense","saving",50,"2018-08-15");
+INSERT INTO transactions (type,category,amount,date) 
+values ("expense","saving",50,"2018-09-15");
+INSERT INTO transactions (type,category,amount,date) 
+values ("expense","saving",50,"2018-10-15");
+
+INSERT INTO transactions (type,category,amount,date) 
+values ("expense","entertainment",86.68,"2018-7-12");
+INSERT INTO transactions (type,category,amount,date) 
+values ("expense","entertainment",41.98,"2018-7-18");
+INSERT INTO transactions (type,category,amount,date) 
+values ("expense","entertainment",56.42,"2018-8-23");
+INSERT INTO transactions (type,category,amount,date) 
+values ("expense","entertainment",28.49,"2018-8-17");
+INSERT INTO transactions (type,category,amount,date) 
+values ("expense","entertainment",39.21,"2018-09-15");
+INSERT INTO transactions (type,category,amount,date) 
+values ("expense","entertainment",25.74,"2018-09-24");
+INSERT INTO transactions (type,category,amount,date) 
+values ("expense","entertainment",13.65,"2018-10-19");
+INSERT INTO transactions (type,category,amount,date) 
+values ("expense","entertainment",103.32,"2018-10-15");
